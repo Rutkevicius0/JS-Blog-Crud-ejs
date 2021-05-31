9ee0ac6034b693057feab7eb20f9fbbf98f6e6c9
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-05-31 09:43:58 +0300

    ejs install, html to ejs , render pages ejs

--- src/index.js
+++ src/index.js
@@ -4,21 +4,28 @@ const path = require('path');
 
 const PORT = 3000;
 
-const kint = 'blue';
+//register view engine
+app.set('view engine', 'ejs');
+// nustatom views direktorija
+app.set('views', 'src/views');
 
 //home page
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+  //   res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+  //paimti index.ejs faila is views direktorijos
+  res.render('index');
 });
 
 //about page
 app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+  //   res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+  res.render('about');
 });
 
 //blog page
 app.get('/blog', function (req, res) {
-  res.sendFile(path.join(__dirname, 'pages', 'blog.html'));
+  //   res.sendFile(path.join(__dirname, 'pages', 'blog.html'));
+  res.render('blog');
 });
 
 const staticPath = path.join(__dirname, 'static');
