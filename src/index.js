c3479a39127b9f828bd641b2b82ffe994b3f2d7a
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-05-31 09:00:42 +0300

    new pages, css, bootstrap

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = 3000;
 
 const kint = 'blue';
 
+//home page
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
+
+//about page
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+});
+
+//blog page
+app.get('/blog', function (req, res) {
+  res.sendFile(path.join(__dirname, 'pages', 'blog.html'));
+});
+
+const staticPath = path.join(__dirname, 'static');
+//statine direktorija css, js ,img ir kitiem statiniam failam
+app.use(express.static(staticPath));
 
 // 404 case - kai vartotojas ivede puslapi kurio nera
 app.use((req, res) => res.status(404).send('OOPs, page not found'));
