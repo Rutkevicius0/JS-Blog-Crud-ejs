9cf109cf4e1edeea73fcdff7e16c1a79ae791864
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-06-03 13:43:54 +0300

    single owner

--- src/routes/ownersRoutes.js
+++ src/routes/ownersRoutes.js
@@ -22,10 +22,10 @@ const ownersArr = [
 //   });
 // });
 
-router.get('/new', (req, res) => {
+router.post('/new', (req, res) => {
   const newOwner = new Owner({
-    name: req.body.name,
-    email: req.body.email,
+    name: 'ovis',
+    email: 'req.body.email',
   });
   newOwner
     .save()
@@ -37,6 +37,12 @@ router.get('/new', (req, res) => {
       });
     })
     .catch((err) => console.log(err));
+});
+router.get('/new', (req, res) => {
+  res.render('owners/new', {
+    title: 'Add owner',
+    page: 'owners_new',
+  });
 });
 
 router.get('/', function (req, res) {
@@ -50,4 +56,29 @@ router.get('/', function (req, res) {
     })
     .catch((err) => console.error(err.message));
 });
+router.get('/single/:id', function (req, res) {
+  const ownerId = req.params.id;
+  Owner.findById(ownerId)
+    .then((result) => {
+      res.render('owners/singleOwner', {
+        title: 'Single owners',
+        page: 'owners',
+        result,
+      });
+    })
+    .catch((err) => console.error(err.message));
+});
+router.get('/single/:id/delete', function (req, res) {
+  const ownerId = req.params.id;
+  Owner.findByIdAndDelete(ownerId)
+    .then((result) => {
+      res.render('owners/delete', {
+        title: 'Single owners',
+        page: 'owners',
+        result,
+      });
+    })
+    .catch((err) => console.error(err.message));
+});
+
 module.exports = router;
