2d4af2fee1ccbf030805c1d86ac5346c78106faf
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-06-04 11:24:02 +0300

    owners controllers done

--- src/controllers/ownersController.js
+++ src/controllers/ownersController.js
@@ -1,5 +1,6 @@
 //owners_index owners_single ...
 const Owner = require('../models/owner');
+
 const owners_index = (req, res) => {
   Owner.find()
     .then((result) => {
@@ -12,4 +13,76 @@ const owners_index = (req, res) => {
     .catch((err) => console.error(err.message));
 };
 
-module.exports = { owners_index };
+const owners_single = (req, res) => {
+  const ownerId = req.params.id;
+  Owner.findById(ownerId)
+    .then((result) => {
+      res.render('owners/singleOwner', {
+        title: 'Single owners',
+        page: 'owners',
+        result,
+      });
+    })
+    .catch((err) => console.error(err.message));
+};
+
+const owners_postNew = (req, res) => {
+  const newOwner = new Owner({
+    name: req.body.name,
+    email: req.body.email,
+  });
+  newOwner
+    .save()
+    .then((result) => {
+      res.render('owners/new', {
+        title: 'Add owner',
+        page: 'owners_new',
+        result,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+const owners_new = (req, res) => {
+  res.render('owners/new', {
+    title: 'Add owner',
+    page: 'owners_new',
+  });
+};
+const owners_deletePost = (req, res) => {
+  Owner.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.redirect('/owners');
+    })
+    .catch((err) => console.error(err.message));
+};
+
+const owners_edit = (req, res) => {
+  const ownerId = req.params.id;
+  Owner.findById(ownerId)
+    .then((result) => {
+      res.render('owners/edit', {
+        title: 'Edit owner',
+        page: 'owners',
+        result,
+      });
+    })
+    .catch((err) => console.error(err.message));
+};
+
+const owners_editPost = (req, res) => {
+  const { title, author, body } = req.body;
+  Owner.findByIdAndUpdate(req.params.id, req.body)
+    .then((result) => {
+      res.redirect('/owners');
+    })
+    .catch((err) => console.log(err));
+};
+module.exports = {
+  owners_index,
+  owners_single,
+  owners_postNew,
+  owners_new,
+  owners_deletePost,
+  owners_edit,
+  owners_editPost,
+};
--- src/routes/ownersRoutes.js
+++ src/routes/ownersRoutes.js
@@ -14,76 +14,21 @@ const ownersControllers = require('../controllers/ownersController');
 // });
 
 //creating nwe owner
-router.post('/new', (req, res) => {
-  const newOwner = new Owner({
-    name: req.body.name,
-    email: req.body.email,
-  });
-  newOwner
-    .save()
-    .then((result) => {
-      res.render('owners/new', {
-        title: 'Add owner',
-        page: 'owners_new',
-        result,
-      });
-    })
-    .catch((err) => console.log(err));
-});
+router.post('/new', ownersControllers.owners_postNew);
 //renddering new owner form page
-router.get('/new', (req, res) => {
-  res.render('owners/index', {
-    title: 'Add owner',
-    page: 'owners_new',
-  });
-});
+router.get('/new', ownersControllers.owners_new);
 
 router.get('/', ownersControllers.owners_index);
 
-router.get('/single/:id', function (req, res) {
-  const ownerId = req.params.id;
-  Owner.findById(ownerId)
-    .then((result) => {
-      res.render('owners/singleOwner', {
-        title: 'Single owners',
-        page: 'owners',
-        result,
-      });
-    })
-    .catch((err) => console.error(err.message));
-});
+router.get('/single/:id', ownersControllers.owners_single);
 // router.get('/delete/:id', function (req, res) {
 //   console.log(req.params.id);
 //   return res.send(req.params.id);
 // });
 
-router.post('/delete/:id', function (req, res) {
-  Owner.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.redirect('/owners');
-    })
-    .catch((err) => console.error(err.message));
-});
+router.post('/delete/:id', ownersControllers.owners_deletePost);
 
-router.get('/edit/:id', function (req, res) {
-  const ownerId = req.params.id;
-  Owner.findById(ownerId)
-    .then((result) => {
-      res.render('owners/edit', {
-        title: 'Edit owner',
-        page: 'owners',
-        result,
-      });
-    })
-    .catch((err) => console.error(err.message));
-});
+router.get('/edit/:id', ownersControllers.owners_edit);
 
-router.post('/edit/:id', (req, res) => {
-  const { title, author, body } = req.body;
-  Owner.findByIdAndUpdate(req.params.id, req.body)
-    .then((result) => {
-      res.redirect('/owners');
-    })
-    .catch((err) => console.log(err));
-});
+router.post('/edit/:id', ownersControllers.owners_editPost);
 module.exports = router;
